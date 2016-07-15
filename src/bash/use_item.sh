d3ccf795b473df17d0dbeda51dbb4285dcf73114
Michael Liquori <mliquori@nerdery.com>
2016-07-14 22:31:42 -0500

    Organization and combinedItems.json

---
+++ src/bash/use_item.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H "apikey: fad7f5c2-703c-49f0-bffa-998486e35221" -H "accept: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 7093364d-5710-e1dd-4e26-df066e9825e3" -d '' "http://thegame.nerderylabs.com:1337/items/use/61207174-513b-4737-97c6-8b44746c5e10"
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 var _ = require('lodash');
+var json2csv = require('json2csv');
 
+// Import cleaned up JSON response Object arrays
 const { day1 } = require('./data/cleaned/day1Array');
 const { day2 } = require('./data/cleaned/day2Array');
 
+// Helpers to massage data
 const extractItemResponses = (responseArray) => responseArray.filter(
     (responseObject) => {
         if (_.get(responseObject, 'Item', null)) {
@@ -19,4 +22,11 @@ const extractItems = (itemResponses) => itemResponses.map(
 const day1Items = extractItems(extractItemResponses(day1));
 const day2Items = extractItems(extractItemResponses(day2));
 
-fs.writeFile('test', JSON.stringify(day1Items));
+const combinedItems = [...day1Items, ...day2Items];
+
+fs.writeFile('./data/combinedItems.json', JSON.stringify(combinedItems), (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`${combinedItems.length} Items written to combinedItems.json`);
+});
